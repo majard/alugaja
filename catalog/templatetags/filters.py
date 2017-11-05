94d34f2c86fe77dc6423eb060874627e84b6803b
unknown <marlon_jardim_@hotmail.com>
2017-11-05 03:34:10 -0200

    Added the distance to the searched location to the houses template.

--- catalog/templatetags/filters.py
+++ catalog/templatetags/filters.py
@@ -4,10 +4,8 @@ from django import template
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
-def house_is_within_distance(context, house):
+def house_distance(context, house):
     distance = context['distance']
     location = context['location']
-    if house.calculate_distance(location) < distance:
-        return True
-    else:
-        return False
+    return house.calculate_distance(location)
+        
