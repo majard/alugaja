2e6dd3c76185b9fefcfb1e4730f62b0d202d273c
unknown <marlon_jardim_@hotmail.com>
2017-11-06 04:36:54 -0200

    Modified the house list template, showing bigger forms elements and added more css.

--- catalog/templatetags/filters.py
+++ catalog/templatetags/filters.py
@@ -4,7 +4,7 @@ from django import template
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
-def house_distance(context, house):
+def distance_to(context, house):
     distance = context['distance']
     location = context['location']
     return house.calculate_distance(location)
