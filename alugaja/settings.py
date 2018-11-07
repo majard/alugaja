4fa109b0240b6f420bd4133ad62d1cf24868afdf
unknown <marlon_jardim_@hotmail.com>
2018-11-07 02:56:39 -0200

    require

--- alugaja/settings.py
+++ alugaja/settings.py
@@ -143,8 +143,11 @@ DISTANCE = 100
 LATITUDE = -22.912194
 LONGITUDE = -43.249910 
 
-GEOS_LIBRARY_PATH = '/app/.geodjango/geos/lib/libgeos_c.so'
-GDAL_LIBRARY_PATH = '/app/.geodjango/gdal/lib/libgdal.so'
+
+from os import environ
+
+GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
