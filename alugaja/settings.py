c118d84ea3a9f75bc85bde1db84df248c2b178ac
unknown <marlon_jardim_@hotmail.com>
2018-11-07 02:30:34 -0200

    require

--- alugaja/settings.py
+++ alugaja/settings.py
@@ -147,5 +147,7 @@ from os import environ
 GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
 GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
 
+WSGI_APPLICATION = 'ProjectName.wsgi.application'
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
